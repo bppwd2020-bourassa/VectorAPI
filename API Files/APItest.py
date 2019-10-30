1bbf9d93feba32e4a01a7ec5eb395e442d8a9943
Bourassa <J.Bourassa@baypath.net>
2019-10-30 14:21:10 -0400

    Update

--- API Files/APItest.py
+++ API Files/APItest.py
@@ -5,7 +5,7 @@ This is a test file to play with the api, I suggest testing using this first
 """
 vector = VectorOBJ("0090185A")
 vector.connect()
-"""
+
 vector.right(90)
 vector.left(90)
 
@@ -27,7 +27,6 @@ vector.streamWAVFile('Pacman-death-sound.wav', 50)
 vector.openCamera(30, True)
 
 print(vector.getStatus("motors_moving"))
-"""
 
 vector.pick_up_cube()
 
